0c00d17bd803acb97cd8e773085da6e550fa5775
geowulf <gwoolf@gmail.com>
2019-08-22 15:37:26 -0700

    Updates

---
+++ precache-manifest.40f8a811865cd3164a62b87f634b6a7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a857b84f1f075f4da361340754c73847",
+    "url": "/kitten-rolodex/index.html"
+  },
+  {
+    "revision": "c9e0db6238bb428a5eef",
+    "url": "/kitten-rolodex/static/css/main.950c173c.chunk.css"
+  },
+  {
+    "revision": "6b76e05d10ca29423204",
+    "url": "/kitten-rolodex/static/js/2.e08e69b1.chunk.js"
+  },
+  {
+    "revision": "c9e0db6238bb428a5eef",
+    "url": "/kitten-rolodex/static/js/main.32694057.chunk.js"
+  },
+  {
+    "revision": "ce0905430b7f9e1ee97c",
+    "url": "/kitten-rolodex/static/js/runtime~main.2e31aa3b.js"
+  }
+]);
